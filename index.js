f1ac8a311c8170c9a202a3938e3b025d32957fc8
Luksonaa18 <lukazhozhadze53@gmail.com>
2025-01-11 13:22:17 +0400

    weathers

---
+++ index.js
@@ -0,0 +1,9 @@
+const x = document.querySelector(".fa-solid")
+const y = document.querySelector(".afteer")
+
+x.addEventListener("click", function () {
+    y.classList.toggle("shown")
+})
+x.addEventListener("click", function () {
+    x.classList.toggle("rotata")
+})
